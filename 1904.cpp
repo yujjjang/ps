7b24afca0e138478c7057eaa331fa88b6190a8e6
yujjjang <yujjjang3@gmail.com>
2017-08-28 12:40:23 +0900

    Create 1904.cpp

---
+++ 1904.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+  vector<int> dp(1000001,0);
+  int n;
+  cin >> n;
+  dp[1]=1;
+  dp[2]=2;
+  for(int i = 3; i <= n; ++i){
+      dp[i]=dp[i-1]+dp[i-2];
+      dp[i]=dp[i]%15746;
+  }
+  cout<<dp[n]<<endl;
+  return 0;
+}
